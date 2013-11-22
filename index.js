efc9632fe14f79cdc638707f6156ed06357b2d03
Thomas Shafer <thomasjshafer@gmail.com>
2013-11-21 21:29:07 -0800

    No more global rendr.server

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 if (!this.window) {
   var Server = require('./server/server')
 
-  exports.Server = Server;
-
-  exports.server = null;
-
   exports.createServer = function(options) {
-    return exports.server = new Server(options);
+    return new Server(options);
   };
 }
